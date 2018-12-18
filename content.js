1bfd09fd7a748ed54cdd2008a5c69be67707cf2e
terfno <sueda.takahito@gmail.com>
2018-12-18 09:42:28 +0900

    add js

---
+++ content.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$('.signup').submit(function(event) {
+		
+		event.preventDefault();
+		$('.can, .field, .rubbish, label, .trash, .reset').addClass('dump');
+		
+		var comment = $('.rubbish').val();
+		$(".field").append("<p class='fullword' data-splitting='chars'>" + comment + "</p>");
+		Splitting();
+		var wordSize = $(".fullword").width();
+		$("body").get(0).style.setProperty("--fieldWidth", wordSize + 'px');
+		$('.fullword').css({
+			'transform'         : 'translateX(' + 400 + 'px) skewX(' + (20) + 'deg)'
+		});
+		$(".char").addClass("sliding");
+		
+	});
+	
+});
+
+$(".rubbish").on({
+	keydown: function(e) {
+		if (e.which === 32)
+		return false;
+	},
+	change: function() {
+		this.value = this.value.replace(/\s/g, "");
+	}
+});
+
+$(".reset").click(function(){
+	$(".dump").removeClass("dump");
+	$( ".fullword" ).remove();
+	$(".signup")[0].reset();
+})
